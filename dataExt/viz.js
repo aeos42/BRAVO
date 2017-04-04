636bc3b87c477806a020b8fc93d9c0dff12e1a04
jheil <julia.heil@colorado.edu>
2017-04-03 19:45:51 -0600

    Update viz.js

--- dataExt/viz.js
+++ dataExt/viz.js
@@ -3,7 +3,7 @@ window.onload = function(){
     document.getElementById("viz1").addEventListener("click", function(){
         chrome.tabs.create({url:chrome.extension.getURL("viz1.html")});});
     document.getElementById("viz2").addEventListener("click", function(){
-        chrome.tabs.create({url:chrome.extension.getURL("viz2.html")});});
+        chrome.tabs.create({url:chrome.extension.getURL("sunburst.html")});});
     document.getElementById("viz3").addEventListener("click", function(){
         chrome.tabs.create({url:chrome.extension.getURL("viz3.html")});});
     document.getElementById("viz4").addEventListener("click", function(){
