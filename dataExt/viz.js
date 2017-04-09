7a9697bc4767aab75ab93d7386cbfa33fbdf4dd2
jheil <julia.heil@colorado.edu>
2017-04-09 15:06:16 -0700

    Add files via upload
    Merged with activetrace basic functionality.
    Combined all data request from viz1, viz3, activetrace, viz4, viz5 into streamgraphData.js and removed redundant modules.
    All Chrome history data requests are 'on-demand' and pull incremental data.
    Added time stamps to background page to test performance of data requests.

--- dataExt/viz.js
+++ dataExt/viz.js
@@ -4,4 +4,13 @@ window.onload = function(){
         chrome.tabs.create({url:chrome.extension.getURL("viz1.html")});});
     document.getElementById("viz2").addEventListener("click", function(){
         chrome.tabs.create({url:chrome.extension.getURL("viz2.html")});});
+    document.getElementById("viz3").addEventListener("click", function(){
+       chrome.tabs.create({url:chrome.extension.getURL("viz3.html")});});
+    document.getElementById("activeTrace").addEventListener("click", function(){
+        chrome.tabs.create({url:chrome.extension.getURL("activeTrace.html")});});
+    document.getElementById("viz4").addEventListener("click", function(){
+        chrome.tabs.create({url:chrome.extension.getURL("viz4.html")});});
+    document.getElementById("viz5").addEventListener("click", function(){
+        chrome.tabs.create({url:chrome.extension.getURL("streamgraph.html")});});
 };
+
